1330f77a5334a1e9a472783de402deb67b3ff046
Nikta Kozlov <yellowswordg@gmail.com>
2020-09-29 16:42:18 +0300

    Initial commit
    Created a base structure for folders. Loaded some photoes to assets.

---
+++ android/app/src/main/kotlin/com/example/travell_recomendation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travell_recomendation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:travell_recomendation/screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
